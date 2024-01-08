f34ffa131bd81dfcb93ffb7903eeaada0df0a65f
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-08 11:09:43 +0100

    posicionamientos de includes en index.php

---
+++ includes/sidebar.php
@@ -0,0 +1,17 @@
+<nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+    <div class="position-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">
+                    Opción 1
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Opción 2
+                </a>
+            </li>
+            <!-- Puedes agregar más opciones de menú aquí -->
+        </ul>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('includes/header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HLC-Cuesta-Enero</title>
-    <!-- CSS -->
-    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
-    <link href="resources/css/style.css" rel="stylesheet">
-    <!-- JS -->
-    <script defer src="resources/js/bootstrap.bundle.min.js"></script>
-    <script defer src="resources/js/script.js"></script>
-</head>
+<div class="container-fluid">
+        <div class="row">
+            <?php include('includes/sidebar.php'); ?>
 
+            <!-- Contenido principal -->
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <!-- Contenido de tu página -->
+                <h1>Bienvenido a la Página de Inicio</h1>
+                <!-- Puedes incluir más contenido aquí -->
+            </main>
+        </div>
+    </div>
 
-<body>
-    <h1 class="text-center mt-4">HLC: Cuesta de Enero</h1>
-</body>
-
-</html>
+<?php include('includes/footer.php'); ?>
